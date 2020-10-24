059a2d6aefda61eced345dcf95806ec2e2c268ac
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-24 20:29:52 +0200

    Add Header component and logo image

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import { LOGO, TITLE } from '../../globals/constants';
+import { useStyles } from './Header.style';
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar className={classes.header} position="static">
+        <Toolbar>
+          <div>
+            <img alt={LOGO.title} className={classes.logo} src={LOGO.src} />
+          </div>
+          <div className={classes.titleWrapper}>
+            <Typography variant="h6">{TITLE}</Typography>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+  header: {
+    backgroundColor: 'white',
+    color: 'black',
+  },
+  logo: {
+    height: '40px',
+    width: '40px',
+  },
+  titleWrapper: {
+    marginLeft: '10px',
+  },
+});
