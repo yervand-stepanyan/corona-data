011d0b10afbbf28636a9079d3aab9f20dc99f80f
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-25 16:01:58 +0100

    Remove React Strict Mode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { ThemeProvider } from '@material-ui/core/styles';
 import Main from './pages/Main';
-import theme from './theme';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Main />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
