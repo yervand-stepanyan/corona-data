1eede323b429acf21312bc63e11e880ec8ff4637
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-25 16:16:59 +0100

    Add localStorage helper to save state

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,8 +1,10 @@
 import logo from '../assets/images/corona-logo_260x260.png';
 
+export const LOCAL_STORAGE = {
+  darkMode: 'darkMode',
+};
 export const LOGO = {
   src: logo,
   title: 'logo',
 };
-
 export const TITLE = 'Corona Data';
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -4,18 +4,24 @@ import Paper from '@material-ui/core/Paper';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-import Header from '../../components/Header';
 import { darkTheme, lightTheme } from '../../theme/theme';
+import Header from '../../components/Header';
+import { loadState, saveState } from '../../helpers/localStorage';
+import { LOCAL_STORAGE } from '../../globals/constants';
 
 import { useStyles } from './Main.style';
 
 function Main() {
   const classes = useStyles();
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    loadState(LOCAL_STORAGE.darkMode) || false
+  );
   const theme = darkMode ? darkTheme : lightTheme;
 
   const handleDarkMode = () => {
     setDarkMode(!darkMode);
+
+    saveState(LOCAL_STORAGE.darkMode, !darkMode);
   };
 
   return (
