86e60e90912b9a13d233e4211cdf42647974278f
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-26 20:57:56 +0100

    Refactor fetchAPI.js

--- src/fetchAPI/fetchAPI.js
+++ src/fetchAPI/fetchAPI.js
@@ -1,5 +1,4 @@
 const coronaURL = `${process.env.REACT_APP_ROOT_API}${process.env.REACT_APP_PARAMS}${process.env.REACT_APP_RAPIDAPI_KEY}`;
-const countriesURL = process.env.REACT_APP_COUNTRIES_API;
 
 async function request({ url, method, body }) {
   const fetchData = body
@@ -20,7 +19,6 @@ async function request({ url, method, body }) {
 }
 
 const API = {
-  getCountriesData: () => request({ url: countriesURL, method: 'GET' }),
   getCoronaData: () => request({ url: coronaURL, method: 'GET' }),
 };
 
