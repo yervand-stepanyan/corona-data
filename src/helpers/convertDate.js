4a1b4f61b85e75b7e54ec93cbaf0673023e3dcf9
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-27 10:02:56 +0100

    Fix empty date bug

--- src/helpers/convertDate.js
+++ src/helpers/convertDate.js
@@ -1,7 +1,10 @@
 export const convertDate = date => {
-  const dateAndTime = date.split(' ');
-  const dateData = dateAndTime[0].split('-');
-  const dateToShow = `${dateData[2]}/${dateData[1]}/${dateData[0]}`;
+  if (date) {
+    const dateAndTime = date.split(' ');
+    const dateData = dateAndTime[0].split('-');
+    const dateToShow = `${dateData[2]}/${dateData[1]}/${dateData[0]}`;
 
-  return `${dateAndTime[1]} ${dateToShow}`;
+    return `${dateAndTime[1]} ${dateToShow}`;
+  }
+  return date;
 };
