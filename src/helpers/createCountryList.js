77ff187e1485b5dd03a584a67347cc37b9e8ddea
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-01 12:40:40 +0100

    Refactor files

--- src/helpers/createCountryList.js
+++ src/helpers/createCountryList.js
@@ -2,6 +2,5 @@ export const createCountryList = array => {
   if (Array.isArray(array) && array.length > 2) {
     return array.slice(1, -1).map(data => ({ name: data.Country_text }));
   }
-
   return array;
 };
